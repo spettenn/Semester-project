4edcdce8e0b74bada6686012ecd46c4217cfe373
spettenn <69841778+spettenn@users.noreply.github.com>
2021-05-20 19:42:44 +0200

    deatisl update push

--- js/details.js
+++ js/details.js
@@ -1,24 +1,40 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const id = params.get('id');
 async function getBlogg() {
-console.log()
+  const id = params.get("id");
+  console.log({ id });
 
-try {
-const response = await fetch('http://api.skole.tech/wp-json/wp/v2/posts/?id=' + id);
-const jsonResults = await response.json();
-const value = jsonResults.id;
+  try {
+    const response = await fetch(
+      `http://api.skole.tech/wp-json/wp/v2/posts/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const jsonResults = await response.json();
+    console.log({ jsonResults });
+    const {
+      slug,
+      content: { rendered: content },
+      title: { rendered: title },
+      better_featured_image,
+    } = jsonResults;
+    const { media_details } = better_featured_image;
 
-document.title = jsonResults.slug;
-document.querySelector('main').innerHTML += `
+    document.title = title;
+    document.querySelector("main").innerHTML += `
 <div class="content">
-    <img src="${value.better_featured_image.media_details.sizes.medium.source_url}" />
-    <h2>${value.slug}</h2>
-    <p>${value.content.rendered}</p>
+    <img src="${media_details.sizes.medium.source_url}" />
+    <h2>${title}</h2>
+    <p>${content}</p>
 </div>
 `;
-} catch (error) {
-} finally {
+  } catch (error) {
+    console.log({ error });
+  } finally {
+  }
 }
-}
-getBlogg(id);
+getBlogg();
