3a9d88ae359a6345f4987d2835d367e0ee67ef36
spettenn <69841778+spettenn@users.noreply.github.com>
2021-05-25 00:09:19 +0200

    small update

--- js/homeCards.js
+++ js/homeCards.js
@@ -7,36 +7,12 @@ async function getItem() {
 		const blogg = jsonResults
 		console.log(blogg);
 
-        const max = 4;
         for (let i = 0; i <= max.length; i++) {
-		blogg(function (value) {
+		blogg.array.forEach(element => {
+            
+        });(function (value) {
 			document.querySelector('main').innerHTML += `
-			<div class="cardOne">
-            <a href="postDetail.html?id=${value.id}">
-            <img class="cardImg" src="${value.better_featured_image.media_details.sizes.medium.source_url}" />
-            <h4 class="cardTitle">${value.title.rendered}</h4>
-            </a>
-            </div>
-            <div class="containerCards">
-            <div class="innerCard">
-            <a href="postDetail.html?id=${value.id}">
-            <h4 class="cardTitle">${value.title.rendered}</h4>
-			<img class="cardImg" src="${value.better_featured_image.media_details.sizes.medium.source_url}" />
-			</a>
-            </div>
-            <div class="innerCard">
-            <a href="postDetail.html?id=${value.id}">
-            <h4 class="cardTitle">${value.title.rendered}</h4>
-			<img class="cardImg" src="${value.better_featured_image.media_details.sizes.medium.source_url}" />
-			</a>
-            </div>
-            </div>
-
-            <div class="spanCard">
-            <img class="cardImg" src="${value.better_featured_image.media_details.sizes.medium.source_url}" />
-            <h4 class="cardTitle">${value.title.rendered}</h4>
-            </div> 
-
+		
         `;
 		});
 	} 
@@ -51,4 +27,3 @@ async function getItem() {
 }
 
 getItem();
-
