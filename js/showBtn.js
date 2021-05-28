8b28693604396d9049f20a81aa116935cca11c00
spettenn <69841778+spettenn@users.noreply.github.com>
2021-05-29 00:21:15 +0200

    some edits

--- js/showBtn.js
+++ js/showBtn.js
@@ -1,15 +1,18 @@
-console.log("potato");
 const button = document.querySelector("#buttonShow");
 console.log(button);
-document.getElementById("buttonShow").onclick = () => {buttonClick()};
-function buttonClick() {
+document.getElementById("buttonShow").onclick = () => {
+  buttonClick();
+};
+function onClick(e) {
   console.log("potatoTwo");
   const cards = document.getElementsByClassName(".card");
   console.log(cards);
-  
-  Array.from(cards).forEach((element,index) => {
-    if (index > 6){
+
+  Array.from(cards).forEach((element, index) => {
+    if (index > 6) {
       element.style = "display: flex;";
+    } else {
+      element.style = "display: none;";
     }
   });
 }
