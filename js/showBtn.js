cbd337491e881bb2da296aa77912bbd32740e2f2
spettenn <69841778+spettenn@users.noreply.github.com>
2021-05-28 11:35:11 +0200

    having issues with hotJar switched to mouseFlow

--- js/showBtn.js
+++ js/showBtn.js
@@ -1,9 +1,15 @@
 console.log("potato");
 const button = document.querySelector("#buttonShow");
 console.log(button);
-document.getElementById("buttonShow").onclick = function() {buttonClick()};
+document.getElementById("buttonShow").onclick = () => {buttonClick()};
 function buttonClick() {
   console.log("potatoTwo");
   const cards = document.getElementsByClassName(".card");
   console.log(cards);
+  
+  Array.from(cards).forEach((element,index) => {
+    if (index > 6){
+      element.style = "display: flex;";
+    }
+  });
 }
