bf50e8de12730d3f30cff0083bf9caa3bea1d8b5
spettenn <69841778+spettenn@users.noreply.github.com>
2021-05-29 18:32:29 +0200

    blogg show more/less works

--- js/scriptBlogg.js
+++ js/scriptBlogg.js
@@ -16,7 +16,7 @@ async function getBlog() {
 			<div class="card" style="display: ${index>=6 ? "none" : "flex"}">
 			<a href="postDetail.html?id=${value.id}">
 			<img class="cardImg" src="${value.better_featured_image.media_details.sizes.medium.source_url}" />
-			<h4 class="cardTitle">${value.title.rendered}</h4>
+			<h4 id="cardTitle">${value.title.rendered}</h4>
 			</a>
 			</div>
         `;
--- js/showBtn.js
+++ js/showBtn.js
@@ -1,18 +1,26 @@
-const button = document.querySelector("#buttonShow");
+const button = document.getElementById("#buttonShow");
 console.log(button);
-document.getElementById("buttonShow").onclick = () => {
-  buttonClick();
-};
-function onClick(e) {
-  console.log("potatoTwo");
-  const cards = document.getElementsByClassName(".card");
+
+function buttonClick(btn) {
+  const isEnabled = btn.classList.contains("enabled");
+  if (isEnabled) {
+    btn.classList.remove("enabled");
+    btn.innerHTML = "Show More";
+  } else {
+    btn.classList.add("enabled");
+    btn.innerHTML = "Show Less";
+  }
+  showCards(!isEnabled);
+}
+function showCards(showAll) {
+  const cards = document.getElementsByClassName("card");
   console.log(cards);
 
   Array.from(cards).forEach((element, index) => {
-    if (index > 6) {
-      element.style = "display: flex;";
-    } else {
+    if (!showAll && index > 5) {
       element.style = "display: none;";
+    } else {
+      element.style = "display: flex;";
     }
   });
 }
